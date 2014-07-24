d9c46a2a5f3ff3f9bb6b62b78067f1af2ab5d43b
Theo Chino <git@theochino.com>
2014-07-24 10:36:55 -0400

    Added the Website under Git

---
+++ Website/public_html/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Article 78 against NYDFS</title>
+<link href="assets/css/styles.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+      
+
+<UL>
+
+<H1>Article 78 against NYFSD</H1>      
+
+<H2>Welcome to the website dedicated to Crowfunding to mount an Article 78 action against NYFSD</H2>
+
+<UL>
+<address>
+	<B>email:</B> article78againstnydfs@gmail.com<BR>
+	<B>twitter:</B> <A HREF="https://twitter.com/A78gnstNYDSF">@A78gnstNYDSF</A><BR>
+	<B>google+:</B> https://plus.google.com/102845644263761409564/posts<BR>
+	<B>facebook:</B> https://www.facebook.com/A78gnstNYDSF<BR>
+	<B>Google Voice:</B> 
+</address>
+</UL>
+
+<h2>What is an Article 78</h2>
+
+<P>
+An Article 78 proceeding serves as a uniform device to challenge the activities of an administra
+tive agency in court. 
+</P>
+
+<UL>
+<A HREF="http://civilservice.sheerinlaw.com/sheerinlaw.com-Article_78_eBook.pdf" TARGET=New>Understanding Article 78 in New York State</A><BR>
+<A HREF="http://www3.law.columbia.edu/hrlr/jlm/chapter-22.pdf" TARGET=New>A Jailhouse Lawyer's Manual - How to Challenge Administrative Decisions Using Article 78 of the NYCPL</A>
+</UL>  
+     
+<h3>Spread the Word</H3>
+
+<UL>
+Twitter: <B>#A78gnstNYDSF</B>
+ 
+<blockquote class="twitter-tweet" lang="en"><p>Website is up and running <a href="http://t.co/5otoymcc7Y">http://t.co/5otoymcc7Y</a> but volunteers badly needed <a href="https://twitter.com/hashtag/A78gnstNYDSF?src=hash">#A78gnstNYDSF</a> <a href="https://twitter.com/hashtag/Bitcoin?src=hash">#Bitcoin</a> <a href="https://twitter.com/hashtag/Article78againstNYDSF?src=hash">#Article78againstNYDSF</a> <a href="https://twitter.com/hashtag/NYDSF?src=hash">#NYDSF</a></p>&mdash; Art78 against NYDFS  (@A78gnstNYDSF) <a href="https://twitter.com/A78gnstNYDSF/statuses/491810211611807744">July 23, 2014</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+Reddit: <A HREF="http://redd.it/2bdq0g">http://redd.it/2bdq0g</A>
+ </UL>
+      
+
+
+<h3>Volunter</h3>
+
+<UL>
+GitHUB: <A HREF="https://github.com/theochino/Article78againstNYDFS">https://github.com/theochino/Article78againstNYDFS</A>
+</UL>
+
+<h3>Donate</h3>
+
+<UL>
+<P>
+	<B>The Goal is 200 BTC.</B>
+</P>
+		
+	<P>
+	Bitcoin:<B><FONT COLOR=RED>178dfs</FONT>WNhT5fT4MBQCEWLWxAZ63kMvkgdW</B>
+  <BR>
+  <A HREF="https://blockchain.info/address/178dfsWNhT5fT4MBQCEWLWxAZ63kMvkgdW">https://blockchain.info/address/178dfsWNhT5fT4MBQCEWLWxAZ63kMvkgdW</A></P>
+
+	<P>
+		<IMG SRC="images/A78gnstNYDSF_bitcoin.png">	
+	</P>
+
+</UL> 
+ 
+<h3>Verify</h3> 
+
+<UL>
+<P>
+This website was paid using a donation of 234.2 mBTC from Theo Chino to Namecheap.com<BR>
+<A HREF="https://blockchain.info/tx/0ed2232fa55245a603ae043594c1824c3428a9a94789a669388601a3722e042b">https://blockchain.info/tx/0ed2232fa55245a603ae043594c1824c3428a9a94789a669388601a3722e042b</A>
+</P>
+ </UL>
+  
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53115128-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+        
+</body>
+</html>
+
